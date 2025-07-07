64b4be3fbf2840391491c8bfd9ab686f2ebbd06f
Nihal Shukla <nihalshukla67952@gmail.com>
2025-07-07 17:50:07 +0530

    Apply Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'assignmentTask';
+
+  assignmentForm: FormGroup = new FormGroup ({
+    checkForm : new FormControl("",[Validators.required]),
+    checkForm1 : new FormControl("",[Validators.required]),
+    shuklaForm : new FormControl("",[Validators.required]),
+    selectOne: new FormControl("",[Validators.required]),
+    mobileNumber : new FormControl("",[Validators.required]),
+    date : new FormControl ("", [Validators.required]),
+    email: new FormControl("", [Validators.required, Validators.email])
+  });
+
+  showAlert(){
+    alert("Please Fill all data")
+  }
 }
